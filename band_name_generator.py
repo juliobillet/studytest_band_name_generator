b4281c5504d3510ebfa45dbf37ac4a332b9f3ec6
Júlio Billet <76001545+juliobillet@users.noreply.github.com>
2021-09-13 14:47:10 -0300

    Add files via upload
    Just a study test with Python.
    
    A simple code to "generate" the name of a band using the name of a city and the name of a pet.

---
+++ band_name_generator.py
@@ -0,0 +1,10 @@
+city_name = ""
+pet_name = ""
+
+print("Welcome to the Band Name Generator Pro \nLet's begin:")
+city_name = input("What is the name of the city you grew up in?\n")
+
+pet_name = input("What is the name of your pet?\n")
+
+#print(f"The name of your band is: {city_name} {pet_name}")
+print("The name of your Band is:", city_name, pet_name)
